49fa5f406303f8fc89df712cbba45840dde01275
Abhilov23 <abhilovgupta01@gmail.com>
2025-04-18 11:53:06 +0530

    fixed tests

--- script/DeployDSC.s.sol
+++ script/DeployDSC.s.sol
@@ -1,26 +1,26 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.19;
 
-import { Script } from "forge-std/Script.sol";
-import { HelperConfig } from "./HelperConfig.s.sol";
-import { DecentralizedStableCoin } from "../src/DecentralizedStableCoin.sol";
-import { DSCEngine } from "../src/DSCEngine.sol";
-
+import {Script} from "forge-std/Script.sol";
+import {HelperConfig} from "./HelperConfig.s.sol";
+import {DecentralizedStableCoin} from "../src/DecentralizedStableCoin.sol";
+import {DSCEngine} from "../src/DSCEngine.sol";
 
 contract DeployDSC is Script {
     address[] public tokenAddresses;
     address[] public priceFeedAddresses;
 
     function run() external returns (DecentralizedStableCoin, DSCEngine, HelperConfig) {
-        HelperConfig helperConfig = new HelperConfig(); // This comes with our mocks!
-
+        HelperConfig helperConfig = new HelperConfig();
         (address wethUsdPriceFeed, address wbtcUsdPriceFeed, address weth, address wbtc, uint256 deployerKey) =
             helperConfig.activeNetworkConfig();
         tokenAddresses = [weth, wbtc];
         priceFeedAddresses = [wethUsdPriceFeed, wbtcUsdPriceFeed];
 
+        // Use msg.sender for ownership in test context
+        address deployer = msg.sender;
         vm.startBroadcast(deployerKey);
-        DecentralizedStableCoin dsc = new DecentralizedStableCoin(msg.sender);
+        DecentralizedStableCoin dsc = new DecentralizedStableCoin(deployer);
         DSCEngine dscEngine = new DSCEngine(tokenAddresses, priceFeedAddresses, address(dsc));
         dsc.transferOwnership(address(dscEngine));
         vm.stopBroadcast();
--- test/unit/DSCEngineTest.t.sol
+++ test/unit/DSCEngineTest.t.sol
@@ -1,14 +1,12 @@
-//SPDX-License-Identifier: MIT
-
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
+
 import {Test} from "forge-std/Test.sol";
 import {DeployDSC} from "../../script/DeployDSC.s.sol";
 import {DecentralizedStableCoin} from "../../src/DecentralizedStableCoin.sol";
 import {DSCEngine} from "../../src/DSCEngine.sol";
 import {HelperConfig} from "../../script/HelperConfig.s.sol";
-import { ERC20Mock } from "@chainlink/contracts/src/v0.8/vendor/openzeppelin-solidity/v4.8.3/contracts/mocks/ERC20Mock.sol";
-
-
+import {ERC20Mock} from "@chainlink/contracts/src/v0.8/vendor/openzeppelin-solidity/v4.8.3/contracts/mocks/ERC20Mock.sol";
 
 contract DSCEngineTest is Test {
     DeployDSC deployer;
@@ -21,19 +19,33 @@ contract DSCEngineTest is Test {
     address public USER = makeAddr("user");
     uint256 public constant AMOUNT_COLLATERAL = 10 ether;
 
-
     function setUp() public {
-        deployer = new DeployDSC();
-        (dsc, dsce, config) = deployer.run();
+        // Reset any prank state
+        vm.stopPrank();
+
+        // Deploy contracts manually to control ownership
+        config = new HelperConfig();
         (ethUsdPriceFeed,, weth,,) = config.activeNetworkConfig();
+
+        // Deploy DecentralizedStableCoin with test contract as owner
+        address testOwner = address(this);
+        dsc = new DecentralizedStableCoin(testOwner);
+
+        // Deploy DSCEngine
+        address[] memory tokenAddresses = new address[](1);
+        address[] memory priceFeedAddresses = new address[](1);
+        tokenAddresses[0] = weth;
+        priceFeedAddresses[0] = ethUsdPriceFeed;
+        dsce = new DSCEngine(tokenAddresses, priceFeedAddresses, address(dsc));
+
+        // Transfer ownership of DecentralizedStableCoin to DSCEngine
+        vm.prank(testOwner);
+        dsc.transferOwnership(address(dsce));
     }
 
-
-    //Price-tests
-
-    function testGetUsdValue()  public{
+    // Price tests
+    function testGetUsdValue() public {
         uint256 ethAmount = 15e18;
-        //15e18/ETH = 30000
         uint256 expectedUsd = 30000e18;
         uint256 actualUsd = dsce.getUsdValue(weth, ethAmount);
         assertEq(expectedUsd, actualUsd);
@@ -42,11 +54,8 @@ contract DSCEngineTest is Test {
     function testRevertsIfCollateralZero() public {
         vm.startPrank(USER);
         ERC20Mock(weth).approve(address(dsce), AMOUNT_COLLATERAL);
-
         vm.expectRevert(DSCEngine.DSCEngine_needMoreThanZero.selector);
         dsce.depositCollateral(weth, 0);
         vm.stopPrank();
     }
-
-    
- }
+}
