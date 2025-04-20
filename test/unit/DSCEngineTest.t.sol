f96f26a7d375f68c79594059fb074b2e577fd967
Abhilov23 <abhilovgupta01@gmail.com>
2025-04-20 20:36:11 +0530

    added test-cases

--- test/unit/DSCEngineTest.t.sol
+++ test/unit/DSCEngineTest.t.sol
@@ -14,34 +14,57 @@ contract DSCEngineTest is Test {
     DSCEngine dsce;
     HelperConfig config;
     address ethUsdPriceFeed;
+    address btcUsdPriceFeed;
     address weth;
 
     address public USER = makeAddr("user");
     uint256 public constant AMOUNT_COLLATERAL = 10 ether;
     uint256 public constant STARTING_ERC20_BALANCE = 10 ether;
 
+    // Public state vars (for shadowing fix)
+    address[] public tokenAddresses;
+    address[] public priceFeedAddresses;
+
     function setUp() public {
         // Reset any prank state
         vm.stopPrank();
 
         config = new HelperConfig();
-        (ethUsdPriceFeed,, weth,,) = config.activeNetworkConfig();
+        (ethUsdPriceFeed, btcUsdPriceFeed, weth,,) = config.activeNetworkConfig();
 
         address testOwner = address(this);
         dsc = new DecentralizedStableCoin(testOwner);
 
-        // Deploy DSCEngine
-        address[] memory tokenAddresses = new address[](1);
-        address[] memory priceFeedAddresses = new address[](1);
-        tokenAddresses[0] = weth;
-        priceFeedAddresses[0] = ethUsdPriceFeed;
-        dsce = new DSCEngine(tokenAddresses, priceFeedAddresses, address(dsc));
+        // Use different names to avoid shadowing state variables
+        address ;
+        address ;
+        _tokenAddresses[0] = weth;
+        _priceFeedAddresses[0] = ethUsdPriceFeed;
+
+        dsce = new DSCEngine(_tokenAddresses, _priceFeedAddresses, address(dsc));
         vm.prank(testOwner);
         dsc.transferOwnership(address(dsce));
+
+        // Give USER some collateral
         ERC20Mock(weth).mint(USER, STARTING_ERC20_BALANCE);
     }
 
-    // Price tests
+    function testRevertsIfTokenLengthDoesntMatchPriceFeeds() public {
+        tokenAddresses.push(weth);
+        priceFeedAddresses.push(ethUsdPriceFeed);
+        priceFeedAddresses.push(btcUsdPriceFeed);
+
+        vm.expectRevert(DSCEngine.DSCEngine_tokenAddressAndPriceFeedEngineMustBeSameLength.selector);
+        new DSCEngine(tokenAddresses, priceFeedAddresses, address(dsc));
+    }
+
+    function testgetTokenAmountFromUsd() public {
+        uint256 usdAmount = 100 ether;
+        uint256 expectedWeth = 0.05 ether;
+        uint256 amountWeth = dsce.getTokenAmountFromUsd(weth, usdAmount);
+        assertEq(amountWeth, expectedWeth);
+    }
+
     function testGetUsdValue() public {
         uint256 ethAmount = 15e18;
         uint256 expectedUsd = 30000e18;
@@ -54,6 +77,14 @@ contract DSCEngineTest is Test {
         ERC20Mock(weth).approve(address(dsce), AMOUNT_COLLATERAL);
         vm.expectRevert(DSCEngine.DSCEngine_needMoreThanZero.selector);
         dsce.depositCollateral(weth, 0);
+        vm.stopPrank();
+    }
+
+    function testRevertsWithUnapprovedCollateral() public {
+        ERC20Mock randToken = new ERC20Mock("RAN", "RAN", USER, AMOUNT_COLLATERAL);
+        vm.startPrank(USER);
+        vm.expectRevert(abi.encodeWithSelector(DSCEngine.DSCEngine_TokenNotAllowed.selector, address(randToken)));
+        dsce.depositCollateral(address(randToken), AMOUNT_COLLATERAL);
         vm.stopPrank();
     }
 }
